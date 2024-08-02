7a224cd2912c749bff534da659c78be16e45df1c
kuetche alex <kuetchealex99@gmail.com>
2024-08-02 09:01:33 +0200

    création du site

---
+++ src/components/Acceuil.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Accueil = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Bienvenue sur notre application !</h1>
+      <p>Nous sommes ravis de vous voir ici. Explorez nos fonctionnalités et profitez de votre expérience.</p>
+    </div>
+  );
+};
+
+export default Accueil;
